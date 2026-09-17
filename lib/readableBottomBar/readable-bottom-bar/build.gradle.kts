c8765a683ad585ad9d28edcf6626423af0b40829
PneumaTech1000 <pneuma.tech1000@gmail.com>
2026-09-17 07:07:15 +0100

    sdk update

--- lib/readableBottomBar/readable-bottom-bar/build.gradle.kts
+++ lib/readableBottomBar/readable-bottom-bar/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.realgear.readable_bottom_bar"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
@@ -24,10 +24,9 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    compileSdkMinor = 0
     buildToolsVersion = "36.0.0"
 
     publishing {
