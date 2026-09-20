c61d567d50fd0658204c88735a59b4422ea5c129
PneumaTech1000 <pneuma.tech1000@gmail.com>
2026-09-20 12:24:19 +0100

    minor update 5

--- app/src/main/java/com/giga/tech1000/heartbeatz/views/MediaPlayerView.java
+++ app/src/main/java/com/giga/tech1000/heartbeatz/views/MediaPlayerView.java
@@ -146,7 +146,7 @@ public class MediaPlayerView {
 
 
     private void init() {
-        playPauseButtonView.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        //playPauseButtonView.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
         albumImageView.setPaletteColor(R.attr.kv_colorSurface);
 
         int surfaceColor = MaterialColors.getColor(rootView.getContext(), R.attr.kv_primaryColor, Color.WHITE);
